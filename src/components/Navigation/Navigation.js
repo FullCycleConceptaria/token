92c8665291b87e4fa73aa2dbf72e530fc2bea9e1
Bluegnarl <dradarkx@gmail.com>
2023-03-11 14:36:17 +0100

    ui

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,7 +4,7 @@ import NavigationLinks from '../NavigationLinks/NavigationLinks';
 export default function Navigation(){
     return(
         <div className='w-full pl-20 pr-20'>
-            <div style={{marginTop: '95px'}} className='b-surface-1 d-flex-row jc-space-between ai-center w-full bora-100'>
+            <div style={{marginTop: '95px', overflowX: 'auto'}} className='b-surface-1 d-flex-row jc-space-between ai-center w-full bora-100'>
                 <NavigationLinks text='Demandes' />
                 <NavigationLinks text='Propositions' />
                 <NavigationLinks text='Groupes' />
