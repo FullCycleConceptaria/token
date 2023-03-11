1f522223ece69c2fab546623b0a26d44e2e6c73f
Bluegnarl <dradarkx@gmail.com>
2023-03-11 09:31:13 +0100

    Header + Footer ended

--- src/components/NavButtons/NavButtons.js
+++ src/components/NavButtons/NavButtons.js
@@ -1,10 +1,8 @@
-import styles from './NavButtons.module.scss'
-
 export default function NavButtons({icon, text}){
     return(
-        <div className='d-flex-column ai-center'>
-            <span className="material-symbols-outlined">{icon}</span>
-            <p className='fs-small fw-500 ff-primary-font'>{text}</p>
+        <div className='d-flex-column ai-center cursor-pointer'>
+            <span className="material-symbols-outlined c-on-surface-variant">{icon}</span>
+            <p className='fs-small fw-500 ff-primary-font c-on-surface-variant'>{text}</p>
         </div>
     )
 }
