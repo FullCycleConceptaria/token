b8cc62654fe37dbde52a874dff152a9153718692
Bluegnarl <dradarkx@gmail.com>
2023-03-11 12:06:41 +0100

    fix navigation

--- src/components/NavigationLinks/NavigationLinks.js
+++ src/components/NavigationLinks/NavigationLinks.js
@@ -2,6 +2,6 @@ import { NavLink } from "react-router-dom";
 
 export default function NavigationLinks({text}){
     return(
-        <NavLink className="c-on-surface-variant ff-primary-font fw-500 pt-8 pl-16 pr-16 pb-8 bora-100" style={{fontSize: '1.4rem'}}>{text}</NavLink>
+        <NavLink className="c-on-surface-variant ff-primary-font fw-500 pt-16 pl-24 pr-24 pb-16 bora-100" style={{fontSize: '1.4rem'}}>{text}</NavLink>
     )
 }
