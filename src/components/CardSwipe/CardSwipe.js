8310413a34a96abf9de02157641c58cf066eaf9c
NiteChill <puissantachille@gmail.com>
2023-03-11 22:46:04 +0100

    totok

--- src/components/CardSwipe/CardSwipe.js
+++ src/components/CardSwipe/CardSwipe.js
@@ -7,9 +7,9 @@ export default function CardSwipe({
   username,
 }) {
   return (
-    <div className='p-20 w-full d-flex-column h-full'>
+    <div className='p-20 w-full d-flex-column h-full' style={{flexGrow: '1', flexShrink: '0'}}>
       <div
-        className='d-flex-column bora-8 b-surface-1 h-full overflow-hidden'
+        className='d-flex-column bora-8 b-surface-1 h-full overflow-hidden pos-relative'
         style={{
           boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.25)',
           maxHeight: '100%',
@@ -19,24 +19,39 @@ export default function CardSwipe({
           className='d-flex-row b-primary-container overflow-hidden'
           style={{ height: '55%', background: imgPresentation }}
         ></div>
-        <div>
-          <img
-            src={imgProfile}
-            alt={username}
-            className='w-full b-primary-container'
-          />
-          <div>
-            <p>{date}</p>
-            <p>{username}</p>
+        <div className='p-16'>
+          <div className='d-flex-row ai-center jc-space-between'>
+            <div className='d-flex-row'>
+              <img src={imgProfile} alt={username} className='w-40 mr-8' />
+              <div>
+                <p className='fs-small ff-primary-font c-on-surface-variant fw-500'>
+                  {date}
+                </p>
+                <p className='fs-normal ff-primary-font c-on-surface fw-500'>
+                  {username}
+                </p>
+              </div>
+            </div>
+            <span
+              className='material-symbols-outlined'
+              style={{ fontSize: '2rem' }}
+            >
+              more_vert
+            </span>
           </div>
-          <span className='material-symbols-outlined'>more_vert</span>
-          <div>
-            <p>{title}</p>
-            <p>{description}</p>
+          <div className='mt-16'>
+            <p className='fs-normal ff-primary-font c-on-surface fw-500'>
+              {title}
+            </p>
+            <p className='fs-small ff-primary-font c-on-surface-variant fw-500 mt-8'>
+              {description}
+            </p>
           </div>
-          <div>
-            <p>Accepter</p>
-          </div>
+        </div>
+        <div className='d-flex-row ai-center jc-center pt-16 pr-24 pb-16 pl-24 bora-100 b-primary-container w-fit-content pos-absolute' style={{bottom: '1.6rem', right: '1.6rem'}}>
+          <p className='fs-normal c-on-primary-container fw-500 ff-primary-font'>
+            Accepter
+          </p>
         </div>
       </div>
     </div>
--- src/pages/home/requests/requests.js
+++ src/pages/home/requests/requests.js
@@ -1,12 +1,23 @@
 import CardSwipe from '../../../components/CardSwipe/CardSwipe';
 import styles from './requests.module.scss';
 import garden from '../../../assets/images/garden.jpg';
+import userPicture from '../../../assets/images/profile-picture.svg';
 
 export default function Requests({}) {
-    return (
-      <div className='d-flex-row h-full' style={{maxHeight: '100%'}}>
-        <CardSwipe imgPresentation={garden} />
-        {/* <CardSwipe imgPresentation={garden} /> */}
-      </div>
-    );
+  return (
+    <div
+      className={`d-flex-row h-full overflow-x-scroll ${styles.scroll}`}
+      style={{ maxHeight: '100%' }}
+    >
+      <CardSwipe
+        imgPresentation={garden}
+        imgProfile={userPicture}
+        date={`Aujourd'hui 15:00`}
+        username='Amélie Dubois'
+        title='bonjour je peux venir en aide pour déblayer un jardin!'
+        description={`mon jardin est dans un piteux état et j'aimerais avoir un peu d'aide car je me fais vieille et je manque de temps.`}
+      />
+      <CardSwipe imgPresentation={garden} />
+    </div>
+  );
 }
