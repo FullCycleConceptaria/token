e3e3d92b847dd26993d025c57000e63b6aadf54f
Bluegnarl <dradarkx@gmail.com>
2023-03-12 11:04:49 +0100

    adding images groups

--- src/pages/home/groups/groups.js
+++ src/pages/home/groups/groups.js
@@ -1,4 +1,7 @@
 import BasicCard from "../../../components/BasicCard/BasicCard";
+import ismv from '../../../assets/images/ismv.svg'
+import hodimont from '../../../assets/images/hodimont.png'
+import rechain from '../../../assets/images/rechain.jpg'
 
 export default function Groups() {
   return (
@@ -6,14 +9,17 @@ export default function Groups() {
       <BasicCard
         title='Groupe de la section informatique'
         text='Juste un groupe de classe comme les autres'
+        img={ismv}
       />
       <BasicCard
         title='Hodimont'
         text="Groupe de la rue d'Hodimont et du terrain d'aventure"
+        img={hodimont}
       />
       <BasicCard
         title='Petit-Rechain'
-        text="Rue Petit-Rechain et son calme absolu"
+        text="La Rue Petit-Rechain et son calme absolu"
+        img={rechain}
       />
     </div>
   );
