c1b88b30ecdab6e8b2c315115d2b7df8e6213b73
NiteChill <puissantachille@gmail.com>
2023-03-12 03:37:19 +0100

    push

---
+++ src/components/BasicCard/BasicCard.js
@@ -0,0 +1,17 @@
+export default function BasicCard({title, text}) {
+  return (
+    <div className='w-full bora-8 b-surface-1 mt-20 overflow-hidden' style={{ height: '30%', boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.25)' }}>
+      <div
+        className='b-primary-container w-full'
+        style={{ height: '60%' }}
+      ></div>
+      <div className='d-flex-column w-full h-full p-16'>
+        <div className='d-flex-row w-full ai-center jc-space-between'>
+          <p className='ff-primary-font c-on-surface fw-500 fs-normal'>{title}</p>
+          <span class='material-symbols-outlined c-on-surface' style={{fontSize: '2rem'}}>more_vert</span>
+        </div>
+        <p className='ff-primary-font c-on-surface-variant fw-400 fs-small mt-8'>{text}</p>
+      </div>
+    </div>
+  );
+}
--- src/pages/home/groups/groups.js
+++ src/pages/home/groups/groups.js
@@ -1,3 +1,20 @@
+import BasicCard from "../../../components/BasicCard/BasicCard";
+
 export default function Groups() {
-  return <p>Groups</p>;
+  return (
+    <div className='w-full h-full pr-20 pb-20 pl-20'>
+      <BasicCard
+        title='Groupe de la setion info'
+        text='juste un groupe de classe comme les autres'
+      />
+      <BasicCard
+        title='Groupe de la setion info'
+        text='juste un groupe de classe comme les autres'
+      />
+      <BasicCard
+        title='Groupe de la setion info'
+        text='juste un groupe de classe comme les autres'
+      />
+    </div>
+  );
 }
